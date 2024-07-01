cc2764cb777095553745f324d641cd6ab6048da5
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-07-01 20:12:52 +0300

    Example of using View model

---
+++ ShopMvcApp_PV212/Models/Contact.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopMvcApp_PV212.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+    }
+}
