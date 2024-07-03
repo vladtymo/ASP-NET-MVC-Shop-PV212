46b065c45a4ccf9fe93bddd6648f437b8454e603
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-07-03 18:54:47 +0300

    Create initial db with products only

---
+++ ShopMvcApp_PV212/Data/ShopDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopMvcApp_PV212.Models;
+
+namespace ShopMvcApp_PV212.Data
+{
+    public class ShopDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ShopMvc_PV212;Integrated Security=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().HasData(new List<Product>()
+            {
+                new Product() { Id = 1, Name = "iPhone X", Category = "Electronics", Discount = 10, Price = 389, Quantity = 4 },
+                new Product() { Id = 2, Name = "Samsung S4", Category = "Electronics", Discount = 0, Price = 440, Quantity = 0 },
+                 new Product() { Id = 3, Name = "LG Smart TV", Category = "TV", Discount = 25, Price = 499, Quantity = 22 }
+            });
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ ShopMvcApp_PV212/Migrations/20240703155328_Initial.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ShopMvcApp_PV212.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Discount = table.Column<int>(type: "int", nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Category", "Description", "Discount", "Name", "Price", "Quantity" },
+                values: new object[,]
+                {
+                    { 1, "Electronics", null, 10, "iPhone X", 389m, 4 },
+                    { 2, "Electronics", null, 0, "Samsung S4", 440m, 0 },
+                    { 3, "TV", null, 25, "LG Smart TV", 499m, 22 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
