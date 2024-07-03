f4962a4c4a22aa3dc386249db7ff6f6d493541f8
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-07-03 18:57:29 +0300

    Load data from db instead of local collection

--- ShopMvcApp_PV212/Controllers/ProductsController.cs
+++ ShopMvcApp_PV212/Controllers/ProductsController.cs
@@ -1,24 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShopMvcApp_PV212.Data;
 using ShopMvcApp_PV212.Models;
 
 namespace ShopMvcApp_PV212.Controllers
 {
-    
     public class ProductsController : Controller
     {
-        List<Product> products;
+        private ShopDbContext ctx = new ShopDbContext();
         public ProductsController()
-        {
-            products = new List<Product>()
-            {
-                new Product() { Id = 1, Name = "iPhone X", Category = "Electronics", Discount = 10, Price = 389, Quantity = 4 },
-                new Product() { Id = 2, Name = "Samsung S4", Category = "Electronics", Discount = 0, Price = 440, Quantity = 0 },
-                 new Product() { Id = 3, Name = "LG Smart TV", Category = "TV", Discount = 25, Price = 499, Quantity = 22 }
-            };
+        { 
         }
+
         public IActionResult Index()
         {
             // .. load data from database ..
+            var products = ctx.Products.ToList();
 
             return View(products);
         }
