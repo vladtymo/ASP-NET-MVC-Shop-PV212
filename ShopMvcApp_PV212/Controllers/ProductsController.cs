40260b6bac7fd43dd80101bdf06ed14f72ad5c3d
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-07-03 18:48:23 +0300

    Create product list page

---
+++ ShopMvcApp_PV212/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopMvcApp_PV212.Models;
+
+namespace ShopMvcApp_PV212.Controllers
+{
+    
+    public class ProductsController : Controller
+    {
+        List<Product> products;
+        public ProductsController()
+        {
+            products = new List<Product>()
+            {
+                new Product() { Id = 1, Name = "iPhone X", Category = "Electronics", Discount = 10, Price = 389, Quantity = 4 },
+                new Product() { Id = 2, Name = "Samsung S4", Category = "Electronics", Discount = 0, Price = 440, Quantity = 0 },
+                 new Product() { Id = 3, Name = "LG Smart TV", Category = "TV", Discount = 25, Price = 499, Quantity = 22 }
+            };
+        }
+        public IActionResult Index()
+        {
+            // .. load data from database ..
+
+            return View(products);
+        }
+    }
+}
---
+++ ShopMvcApp_PV212/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace ShopMvcApp_PV212.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public int Discount { get; set; }
+        public string Category { get; set; }
+        public int Quantity { get; set; }
+    }
+}
